bf577d4006608ef56e81aa51fdaa37c9af2fe1e3
Ian Williamson <ian.williamson@utexas.edu>
2015-10-23 10:20:22 -0500

    Improved error handling in mphload_enhanced

--- mphload_enhanced.m
+++ mphload_enhanced.m
@@ -1,13 +1,25 @@
-function [m, model_fn]  = mphload_enhanced( model_fn )
+function [m, model_fn]  = mphload_enhanced( model_fn, varargin )
 import com.comsol.model.*
 import com.comsol.model.util.*
-try
-    m=mphload(model_fn);
-catch err
-    [FileName,PathName]=uigetfile('./*.mph');
-    model_fn=[PathName,FileName];
-    m=mphload(model_fn);
+
+if nargin>1
+    model_tag = varargin{1};
+else
+    [~,model_tag]=fileparts(model_fn);
 end
-fprintf('(#) Successfully loaded COMSOL model from ''%s''\n',model_fn);
+
+try
+    m=mphload(model_fn,model_tag);
+catch err
+    if ~isempty( strfind(err.message,'could not be found') )
+        [FileName,PathName]=uigetfile('./*.mph');
+        model_fn=[PathName,FileName];
+        m=mphload(model_fn,model_tag);
+    else
+        err.rethrow();
+    end
+end
+fprintf('# Loaded ''%s''\n',model_fn);
+fprintf('# Assigned model tag: ''%s''\n',model_tag);
 end
 
